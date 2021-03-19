e102bd4d836b7a2952bafa43e79315bf2501225f
iamrohitpc555 <35221090+iamrohitpc555@users.noreply.github.com>
2021-03-20 01:14:47 +0530

    Add files via upload

---
+++ SwitchCase/SingleDigit.js
@@ -0,0 +1,40 @@
+const prompt = require("prompt-sync")();
+let Digit = prompt('Enter the Digit : ');
+ Digit = Number(Digit);
+
+ switch (Digit)
+ {
+
+    case 1:
+        console.log("Sunday")
+        break;
+
+    case 2:
+        console.log("Monday")
+        break;
+
+    case 3:
+        console.log("Thusday")
+        break;
+
+    case 4:
+        console.log("Wednesday")
+        break;
+    
+    case 5:
+        console.log("Thursday")
+        break;
+
+    case 6:
+        console.log("Friday")
+        break;
+
+    case 7:
+        console.log("Saturday");
+        break;
+    
+
+    default:
+        console.log("Invalid Input")
+        break;
+ }
