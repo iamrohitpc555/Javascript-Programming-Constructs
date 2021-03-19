edd20b5fd6e58266917a396e7793fcd601deb9f0
iamrohitpc555 <35221090+iamrohitpc555@users.noreply.github.com>
2021-03-19 23:26:42 +0530

    Add files via upload

---
+++ Sequence/Dice.js
@@ -0,0 +1,3 @@
+// Returns a value between 0-5 and then adding 1 to make range 1-6
+let diceNum=Math.floor(Math.random()*6)+1;
+console.log(diceNum);
---
+++ Sequence/SingleDigit.js
@@ -0,0 +1,3 @@
+//random function to get single digit
+let num = Math.floor(Math.random()*10);
+console.log(num);
